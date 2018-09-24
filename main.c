dadaf6c67dcc2955195a4f5d96c7398ecbcfe3ce
lshenbagaraj <Lakshminarayanan_Shenbagaraj@comcast.com>
2018-09-24 15:38:32 +0530

    some ui messages are being added

--- main.c
+++ main.c
@@ -2,4 +2,8 @@
 #include<conio.h>
 void main()
 {printf("There is a super calc at work");
+printf("hi drishya");
+printf("what's up");
+printf("do u want to do calculations");
+printf("you want to add or subtract");
 }
