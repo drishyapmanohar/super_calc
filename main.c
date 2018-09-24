14e1c1f58e8d75cc2f7e80b70b96cac6024b2465
lshenbagaraj <Lakshminarayanan_Shenbagaraj@comcast.com>
2018-09-24 15:50:28 +0530

    message

--- main.c
+++ main.c
@@ -1,18 +1,15 @@
 #include<stdio.h>
 #include<conio.h>
 void main()
-{printf("There is a super calc at work");
-
+{
+printf("There is a super calc at work");
 printf("hi drishya");
 printf("what's up");
 printf("do u want to do calculations");
 printf("you want to add or subtract");
-}
-
-
+printf("so you want to do addition")
 int n, i, sum = 0;
-    
-    printf("Enter a positive integer: ");
+printf("Enter a positive integer: ");
     scanf("%d",&n);
 
     for(i=1; i <= n; ++i)
