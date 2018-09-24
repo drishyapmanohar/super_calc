a3ebc393a3b8b78b24c4b40a6d7d82e4ef4aec28
lshenbagaraj <Lakshminarayanan_Shenbagaraj@comcast.com>
2018-09-24 15:56:23 +0530

    hsfhg

--- main.c
+++ main.c
@@ -21,21 +21,7 @@ printf("Enter a positive integer: ");
 
     return 0;
 
-int a,b,sub;
 
-	//Read value of a
-	printf("Enter the first no.: ");
-	scanf("%d",&a);
-
-	//Read value of b
-	printf("Enter the second no.: ");
-	scanf("%d",&b);
-
-	//formula of subtraction
-	sub= a-b;
-	printf("subtract is = %d\n", sub);
-
-	return 0;
 
 }
 
