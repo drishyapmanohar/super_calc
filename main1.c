a1b5785846272a80748579988286f333388cd267
lshenbagaraj <Lakshminarayanan_Shenbagaraj@comcast.com>
2018-09-25 15:59:56 +0530

    add

---
+++ main1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+printf("There is a super calc at work");
+printf("hi drishya");
+printf("what's up");
+printf("do u want to do calculations");
+printf("you want to add or subtract");
+
+int a,b,add;
+
+	//Read value of a
+	printf("Enter the first no.: ");
+	scanf("%d",&a);
+
+	//Read value of b
+	printf("Enter the second no.: ");
+	scanf("%d",&b);
+
+	//formula of subtraction
+	add= a+b;
+	printf("subtract is = %d\n", add);
+
+	return 0;
+}
