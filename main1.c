39e772edd73dbdf0ee1e61ab62f0c4acbeb0505d
lshenbagaraj <Lakshminarayanan_Shenbagaraj@comcast.com>
2018-09-25 10:42:34 +0530

    sub two nos

--- main1.c
+++ main1.c
@@ -20,7 +20,21 @@ printf("Enter a positive integer: ");
     printf("Sum = %d",sum);
 
     return 0;
+int a,b,sub;
 
+	//Read value of a
+	printf("Enter the first no.: ");
+	scanf("%d",&a);
+
+	//Read value of b
+	printf("Enter the second no.: ");
+	scanf("%d",&b);
+
+	//formula of subtraction
+	sub= a-b;
+	printf("subtract is = %d\n", sub);
+
+	return 0;
 
 
 }
