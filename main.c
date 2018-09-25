e2c13b8994b5cf2cacaa6e1867895919c88cf72e
lshenbagaraj <Lakshminarayanan_Shenbagaraj@comcast.com>
2018-09-25 16:02:30 +0530

    add1

--- main.c
+++ main.c
@@ -7,21 +7,8 @@ printf("hi drishya");
 printf("what's up");
 printf("do u want to do calculations");
 printf("you want to add or subtract");
-printf("so you want to do addition")
-int n, i, sum = 0;
-printf("Enter a positive integer: ");
-    scanf("%d",&n);
 
-    for(i=1; i <= n; ++i)
-    {
-        sum += i;   // sum = sum+i;
-    }
-
-    printf("Sum = %d",sum);
-
-    return 0;
-
-int a,b,sub;
+int a,b,add;
 
 	//Read value of a
 	printf("Enter the first no.: ");
@@ -32,11 +19,8 @@ int a,b,sub;
 	scanf("%d",&b);
 
 	//formula of subtraction
-	sub= a-b;
-	printf("subtract is = %d\n", sub);
+	add= a+b;
+	printf("subtract is = %d\n", add);
 
 	return 0;
-
 }
-
-
