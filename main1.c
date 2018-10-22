9dc93733dd658fc2df60b527ea5256c4b26fdd4d
saidurga95 <saidurga.pothula95@gmail.com>
2018-10-22 15:43:09 +0530

    2nd commit in master

--- main1.c
+++ main1.c
@@ -3,7 +3,7 @@
 void main()
 {
 printf("There is a super calc at work");
-printf("hi durga");
+printf("hi saidurga");
 printf("what's up");
 printf("do u want to do calculations");
 printf("you want to add or subtract");
